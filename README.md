1d081a9e95da7b0b392f0b5a0a5e209939168c7c
Kyle Cui <kcui@kyles-mbp.devices.brown.edu>
2019-12-06 16:15:13 -0500

    more preprocessing

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ then activate with `conda activate movie-classification`.
 
 Step 2: Run `python download-filmgrab.py` to scrape film images from filmgrab.com. The images are stored as `./filmgrab/<movie_name>/#.jpg` and a list of titles is stored in `data/movie-titles.txt`.
 
-Step 3: Download and extract the IMDb movie info `title.basics.tsv` (with genre data) from https://datasets.imdbws.com/title.basics.tsv.gz and place in the root directory, then run `parse-imdb-data.py`. This will store a map from title to IMDb-assigned genre in `data/genre-map.txt` as a tsv.
+Step 3: Download and extract the IMDb movie info `title.basics.tsv` (with genre data) from https://datasets.imdbws.com/title.basics.tsv.gz and place in the root directory, then run `parse-imdb-data.py`. This will store a map from title to IMDb-assigned genre in `data/genre-map.txt` as a tsv and a map from movie frame path (relative) to genre (just one if `only_first_genre` is True, otherwise a comma-separated list of genres) in `data/frame-genre-map.txt`. This is the dataset we'll be using.
--- parse-imdb-data.py
+++ parse-imdb-data.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import unicodedata
 import os
@@ -8,10 +9,20 @@ try:
 except:
     os.mkdir(base_data_path)
 
-imdb_data_url = 'https://datasets.imdbws.com/title.basics.tsv.gz'
+parser = argparse.ArgumentParser(description='IMDb data parser and mapper')
+parser.add_argument('--imdb_data_url', type=str, help="url to scrape IMDb tsv information", default='https://datasets.imdbws.com/title.basics.tsv.gz', required=False)
+parser.add_argument('-f', '--frames_path', type=str, help="input folder of movie frames", default="./film-grab", required=False)
+args = parser.parse_args()
+
+imdb_data_url = args.imdb_data_url
 imdb_data_path = './title.basics.tsv'
 movie_titles_path = './data/movie-titles.txt'
 genre_map_path = './data/genre-map.txt'
+frame_genre_map_path = './data/frame-genre-map.txt'
+
+frames_path = args.frames_path
+
+genre_map_dict = dict()
 
 def format_title(title):
     illegal_chars = "'/:;,.!?&"
@@ -35,12 +46,50 @@ def construct_genre_map():
                     titles.remove(potential_title)
                     if row['genres'] != "\\N":
                         genre_map.write("%s\t%s\n" % (potential_title, row['genres']))
+                        genre_map_dict[potential_title] = row['genres']
 
         # print(len(titles))
         print("warning: unable to get genre information for %d titles; these will be skipped" % len(titles))
     print("finished constructing genre map (stored in data/genre-map.txt).")
 
+def populate_genre_map_dict(only_first_genre=False):
+    print("populating genre dict...")
+    try:
+        with open(genre_map_path) as map:
+            for line in map:
+                movie_title, genres = line.split('\t')
+                if only_first_genre:
+                    genre_map_dict[movie_title] = genres.split(',')[0].strip()
+                else:
+                    genre_map_dict[movie_title] = genres.strip()
+        print("finished populating genre dict.")
+    except:
+        print("error: genre-map.txt could not be found; aborting")
+
+
+def construct_frame_genre_map(only_first_genre=False):
+    """
+    Writes a text file of frame path to genre.
+    """
+    if len(genre_map_dict) == 0:
+        populate_genre_map_dict()
+    print("constructing frame genre map...")
+    with open(frame_genre_map_path, 'w') as map:
+        for (dirpath, _, filenames) in os.walk(frames_path):
+            for filename in filenames:
+                movie_title = os.path.basename(dirpath)
+                if movie_title in genre_map_dict:
+                    map.write("%s\t%s\n" % (os.path.join(dirpath, filename), genre_map_dict[movie_title]))
+    print("done constructing frame genre map (stored in data/frame-genre-map.txt).")
+    
+
+
 if __name__ == "__main__":
     pass
     # UNCOMMENT TO RUN
-    construct_genre_map()
+    try:
+        os.stat(genre_map_path)
+        print("genre map exists already!")
+    except:
+        construct_genre_map()
+    construct_frame_genre_map()
