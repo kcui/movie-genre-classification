6f6d9777ff4a3291b23b6274a45f26be37e34699
Kyle Cui <kcui@Kyles-MBP.fios-router.home>
2019-12-06 21:01:31 -0500

    preprocess.py

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Step 1: Initialize an environment from the given .yml by running
 
 then activate with `conda activate movie-classification`.
 
-Step 2: Run `python download-filmgrab.py` to scrape film images from filmgrab.com. The images are stored as `./filmgrab/<movie_name>/#.jpg` and a list of titles is stored in `data/movie-titles.txt`.
+Step 2: Run `python download-filmgrab.py` to scrape film images from filmgrab.com. The images are stored as `./filmgrab/<movie_name>/#.jpg` and a list of titles is stored in `data/movie-titles.txt`. Alternatively, download the prescraped dataset
+
+https://www.dropbox.com/s/e1ip9pldog8mff8/filmgrab.tar.gz?dl=0&fbclid=IwAR3lA_WjcvMZYYUsPI9PcwrZq_wOPpdN2I6gpamtRZaSCSA_MqE6ob4FcjY
+
+and run only `write_movie_titles()` in `main()`.
 
 Step 3: Download and extract the IMDb movie info `title.basics.tsv` (with genre data) from https://datasets.imdbws.com/title.basics.tsv.gz and place in the root directory, then run `parse-imdb-data.py`. This will store a map from title to IMDb-assigned genre in `data/genre-map.txt` as a tsv and a map from movie frame path (relative) to genre (just one if `only_first_genre` is True, otherwise a comma-separated list of genres) in `data/frame-genre-map.txt`. This is the dataset we'll be using.
---
+++ preprocess.py
@@ -0,0 +1,52 @@
+import argparse
+import csv
+import unicodedata
+import os
+import tensorflow as tf
+
+def load_framedata(path="./data/frame-genre-map.txt", drop_rate=0, train_ratio=1):
+    # TODO: implement drop_rate, test/train split
+    # check if the mapping file is available
+    try:
+        os.stat(path)
+    except:
+        print("error: frame to genre map file not found; aborting")
+        return None
+    
+    inputs = []
+    labels = []
+    # total number of frames ~130k, takes about 30min?
+    with open(path) as map:
+        # iterate over each frame -> genre mapping and get array rep of jpgs as inputs,
+        # genre as labels
+        for i, mapping in enumerate(map):
+            frame_path, genre = mapping.split('\t')
+            inputs.append(load_and_process_image(frame_path))
+            labels.append(genre)
+            if i % 1000 == 0:
+                print("processed %d frames" % i)
+        return inputs, labels
+
+def load_and_process_image(file_path):
+    new_size = [100, 185] # dunno if this is good
+
+    image = tf.io.decode_jpeg(tf.io.read_file(file_path), channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    # resize image
+    image = tf.image.resize(image, new_size)
+    return image
+
+inputs, labels = load_framedata()
+
+
+# image = tf.image.convert_image_dtype(image, tf.float32)
+# resize image
+# image = tf.image.resize(image, [100, 185], method='nearest')
+# enc = tf.image.encode_jpeg(image)
+# fname = tf.constant('asdf.jpg')
+# fwrite = tf.io.write_file(fname, enc)
+# return image
+    
+    
+
+    
