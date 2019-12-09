20150f61969f22cbdb25691f4c5966e7ae14b664
angelcch3 <angelcch3@gmail.com>
2019-12-09 02:52:24 -0500

    angel debugging for model

--- preprocess.py
+++ preprocess.py
@@ -30,15 +30,12 @@ def load_framedata(path="./data/frame-genre-map.txt", train_ratio=1):
         # genre as labels
         for i, mapping in enumerate(map):
             frame_path, genres = mapping.split('\t')
-            print('hi')
-            print(genres)
             genres = genres.strip()
             genres = genres.split(',')
 
             inputs.append(frame_path)
             labels.append(genres[0])
             #labels.append([genres[1]])
-            print(labels) # just getting the first genre for now
 
         # onehotencoding labels, needs to be editted for multiclass
     enc = OneHotEncoder()
