047fec5414c415527be4533a9aac05f054c6b02a
angelcch3 <angelcch3@gmail.com>
2019-12-09 02:54:04 -0500

    angel debugging for model

--- preprocess.py
+++ preprocess.py
@@ -34,7 +34,7 @@ def load_framedata(path="./data/frame-genre-map.txt", train_ratio=1):
             genres = genres.split(',')
 
             inputs.append(frame_path)
-            labels.append(genres[0])
+            labels.append([genres[0]])
             #labels.append([genres[1]])
 
         # onehotencoding labels, needs to be editted for multiclass
